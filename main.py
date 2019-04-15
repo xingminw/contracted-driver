56322ff5e8966968356929f91a38cb31a2ba4fb2
Xingmin Wang <xingminw@umich.edu>
2019-04-15 16:48:32 -0400

    aha

--- main.py
+++ main.py
@@ -6,10 +6,34 @@ import numpy as np
 
 def main():
     # load the network
-    network = initiate_network(reload=False)
+    network = initiate_network(reload=True)
+
+    # plt.figure()
+    # for driver_id in network.drivers.keys():
+    #     drivers = network.drivers[driver_id]
+    #     driver_prefer_link = drivers.links_preference
+    #
+    #     plt.plot(driver_prefer_link, ".-", label="Driver Class " + str(driver_id))
+    #
+    # plt.legend()
+    # plt.xticks(np.linspace(0, 23, 24), [str(int(val + 1)) for val in np.linspace(0, 23, 24)])
+    # plt.xlim([-0.5, 23.5])
+    # plt.ylabel("Link-based Cost of Drivers ($)")
+    # plt.xlabel("Hour")
+    # plt.show()
+    # exit()
 
     contracted_path = [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
     base_solution = get_lower_level_solution(network, contracted_path, 0, [0.0, 0.0, 0.0])
+
+    # base_demand = base_solution["base_demand"]
+    # plt.figure()
+    # plt.plot([val / 1000 for val in base_demand], ".-")
+    # plt.ylabel("Base demand (# of trips, $10^3$)")
+    # plt.xlabel("Hour")
+    # plt.xticks(np.linspace(0, 23, 24), [str(int(val + 1)) for val in np.linspace(0, 23, 24)])
+    # plt.xlim([-0.5, 23.5])
+    # plt.show()
     # exit()
 
     contracted_fracs = [0, 0, 0.3]
@@ -26,7 +50,6 @@ def main():
     plt.plot(bonus_list, platform_benefit_list, ".-")
     plt.xlabel("Additional bonus for the contraction ($)")
     plt.ylabel("Platform benefit ($)")
-    plt.xticks(np.linspace(0, 23, 24), [str(int(val)) for val in np.linspace(0, 23, 24)])
     plt.show()
 
 
